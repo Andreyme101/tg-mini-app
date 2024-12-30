1f1245c45588806ba7e4c3370dbccf4d91aa67f9
Андрей Рыбалко <andreyrybalko27@gmail.com>
2024-12-30 20:23:38 +0300

    Initial commit for Heroku deploy

---
+++ bot.py
@@ -0,0 +1,36 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.filters import CommandStart
+import asyncio
+
+# Замените на токен вашего бота
+TOKEN = "8070699664:AAEMGr3TM0KsvGTfkwqSASaWhZQc1sKOg2w"
+
+# Создание бота и диспетчера
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+# Команда старт
+@dp.message(CommandStart())
+async def start_command(message: types.Message):
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Открыть Mini App",
+                    web_app=WebAppInfo(url="https://b94c-178-66-129-153.ngrok-free.app")
+                )
+            ]
+        ]
+    )
+    await message.answer(
+        "Привет! Это Mini App. Нажми кнопку ниже, чтобы начать 🚀",
+        reply_markup=keyboard
+    )
+
+# Функция запуска бота
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
